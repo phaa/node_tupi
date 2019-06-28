78b7fdfe070dd625857962127817cf8192cc02a4
phaa <pedrohamorim2011@hotmail.com>
2019-06-28 20:15:29 -0300

    Mod no css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,17 @@
 let mix = require('laravel-mix');
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
+//mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/');
 
 // Style files
 mix.styles([
     'public/vendor/animate/animate.css',
     'public/vendor/slick/slick.css',
     'public/vendor/lightbox/css/lightbox.css',
-    'public/vendor/css-hamburgers/hamburgers.css'
-], 'public/css/vendor.min.css').minify();
+    //'public/vendor/css-hamburgers/hamburgers.css'
+], 'public/css/vendor.min.css');
 
 // Vendor JS minifying
 mix.scripts([
     'public/vendor/parallax100/parallax100.js',
     'public/vendor/lightbox/js/lightbox.min.js'
-], 'public/js/vendor.min.js').minify();
+], 'public/js/vendor.min.js');
